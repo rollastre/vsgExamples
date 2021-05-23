e2d1580c25b24239b83d29c38fa3ffcc73b33462
kjf <superbumbot@gmail.com>
2021-05-23 15:59:30 +0200

    ios empty app starts up from xcode debugger

---
+++ examples/platform/vsgiosnative/vsg_iOS_ApplicationDelegate.h
@@ -0,0 +1,19 @@
+//
+//  vsg_iOS_ApplicationDelegate.h
+//  IOS_vsg_native_example
+//
+//  Created by jaume dominguez faus on 23/5/21.
+//
+
+#ifndef vsg_iOS_ApplicationDelegate_h
+#define vsg_iOS_ApplicationDelegate_h
+
+#import <UIKit/UIKit.h>
+
+@interface vsg_iOS_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
+#endif /* vsg_iOS_ApplicationDelegate_h */
